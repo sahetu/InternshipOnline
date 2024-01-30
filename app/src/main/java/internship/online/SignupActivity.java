5273f932e76e1a8bf21c7037578120160be9cdeb
Sagar Khatri <khatrisagar2@gmail.com>
2024-01-30 18:11:19 +0530

    Signup Java Commit

--- app/src/main/java/internship/online/SignupActivity.java
+++ app/src/main/java/internship/online/SignupActivity.java
@@ -2,12 +2,22 @@ package internship.online;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -15,6 +25,16 @@ public class SignupActivity extends AppCompatActivity {
     Button signup;
 
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    //RadioButton male,female;
+    RadioGroup gender;
+    String sGender = "";
+
+    Spinner city;
+    //String[] cityArray = {"Select City","Ahmedabad","Vadodara","Surat","Rajkot","Junagadh"};
+    ArrayList<String> cityArray;
+
+    CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +48,70 @@ public class SignupActivity extends AppCompatActivity {
         password = findViewById(R.id.signup_password);
         confirmPassword = findViewById(R.id.signup_confirm_password);
         signup = findViewById(R.id.signup_button);
+
+        checkBox = findViewById(R.id.signup_checkbox);
+
+        city = findViewById(R.id.spinner_city);
+
+        cityArray = new ArrayList<>();
+        cityArray.add("Select City");
+        cityArray.add("Ahmedabad");
+        cityArray.add("Rajkot");
+        cityArray.add("Vadodara");
+        cityArray.add("Demo");
+        cityArray.add("Test");
+
+        cityArray.remove(4);
+        cityArray.set(4,"Surat");
+
+        ArrayAdapter adapter = new ArrayAdapter(SignupActivity.this, android.R.layout.simple_list_item_1,cityArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_checked);
+        city.setAdapter(adapter);
+
+        city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i==0){
+
+                }
+                else {
+                    //new CommonMethod(SignupActivity.this, cityArray[i]);
+                    new CommonMethod(SignupActivity.this, cityArray.get(i));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+        gender = findViewById(R.id.signup_gender);
+
+        gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton rb = findViewById(i);
+                sGender = rb.getText().toString();
+                new CommonMethod(SignupActivity.this,sGender);
+            }
+        });
+
+        /*male = findViewById(R.id.signup_male);
+        female = findViewById(R.id.signup_female);
+
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new CommonMethod(SignupActivity.this,male.getText().toString());
+            }
+        });
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new CommonMethod(SignupActivity.this,female.getText().toString());
+            }
+        });*/
 
         password.addTextChangedListener(new TextWatcher() {
             @Override
@@ -118,6 +202,18 @@ public class SignupActivity extends AppCompatActivity {
                 }
                 else if(!password.getText().toString().trim().matches(confirmPassword.getText().toString().trim())){
                     confirmPassword.setError("Password Does Not Match");
+                }
+                /*else if(sGender.trim().equals("")){
+                    new CommonMethod(SignupActivity.this,"Please Select Gender");
+                }*/
+                else if(gender.getCheckedRadioButtonId() == -1){
+                    new CommonMethod(SignupActivity.this,"Please Select Gender");
+                }
+                else if(city.getSelectedItemPosition()<=0){
+                    new CommonMethod(SignupActivity.this,"Please Select City");
+                }
+                else if(!checkBox.isChecked()){
+                    new CommonMethod(SignupActivity.this,"Please Accept Terms & Conditions");
                 }
                 else{
                     new CommonMethod(SignupActivity.this,"Signup Successfully");
